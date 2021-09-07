f9cb82c3c695f366a4b0ddda3f838cd8f4744368
adamiweb <90262638+adamiweb@users.noreply.github.com>
2021-09-08 03:28:08 +0800

    Update jquery.timeline.js

--- tmpda/src/jquery.timeline.js
+++ tmpda/src/jquery.timeline.js
@@ -62,6 +62,7 @@
         var self = this, data = this.options.transformer(response);
 
         Object.keys(data).forEach(function (year) {
+            alert(year);
            self.createYearLabel(year).appendTo(self.options.container);
 
            var $wrapper = $('<div />').addClass('timeline-item-wrapper');
